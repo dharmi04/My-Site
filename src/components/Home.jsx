ae3698de7c6e41da6a495629990bb8d814501d5e
Codder12346768 <dharmip060@gmail.com>
2023-11-22 20:33:46 +0530

    Style improved

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,36 +1,55 @@
 import React from 'react';
-import { TypeAnimation } from 'react-type-animation';
-import blob from '../assets/girlvector.webp';
-import AnimatedBackground from './AnimatedBackground'; 
-import './Home.css'; 
+import Particles from 'react-particles';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div className='flex md:flex-row flex-col h-screen '>
-      <div className="flex flex-col justify-center md:pl-20 pt-10 md:w-2/4">
-        <h1 className="text-white font-bold md:text-5xl text-3xl items-center justify-center tracking-wide">DHARMI PATEL</h1>
-        <p className="text-white font-semibold text-2xl md:text-4xl mt-5">
-          <TypeAnimation
-            sequence={[
-              'CyberSecurity Enthusiast',
-              1300,
-              'Front-End Developer',
-              1300,
-            ]}
-            speed={30}
-            wrapper="span"
-            repeat={Infinity}
-            cursor={true}
-          />
-        </p>
-        <p className='text-white md:text-3xl text-xl mt-5 leading-20'>I am the digital architect, weaving pixelated dreams into vibrant realities. As a front-end developer, I am the conductor of the symphony that is user experience. With code as my instrument, I sculpt seamless interfaces, ensuring every click and scroll is a harmonious melody of design and functionality.</p>
-      </div>
-      <div className="flex md:flex-row flex-col  md:w-2/4 mt-5 md:mt-20 ml-20 md:ml-0">
-        <img src={blob} alt="Blob" className='h-[300px] w-[300px] md:h-[600px] md:w-[600px]'/>
+    <div className='relative h-screen overflow-hidden'>
+      {/* Animated background using react-particles-js */}
+      {/* <Particles
+        params={{
+          particles: {
+            number: {
+              value: 80,
+            },
+            size: {
+              value: 3,
+            },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: 'repulse',
+              },
+            },
+          },
+        }}
+        className="absolute w-full h-full"
+      /> */}
+
+      <div className='flex justify-center items-center h-full'>
+        
+        <div className='text-white md:text-3xl text-lg mt-5 leading-7 flex-col space-y-5 items-center w-3/4'>
+        <h1 className='text-white md:text-4xl text-xl border-b-4 text-center font-bold p-3 '>Exploring Tech and Security</h1>
+          <p className='text-center'>
+            Hello, I'm Dharmi Patel, a dedicated front-end developer. Beyond the world of code, I harbor a keen
+            interest in cybersecurity.
+          </p>
+
+          <p className='text-center'>
+            As a team player, I bring technical expertise and a commitment to perfection. Challenges are opportunities for innovation, and I thrive in collaborative environments, propelling us toward shared goals.
+          </p>
+
+          <p className='text-center'>
+            Let's connect and explore the possibilities at the intersection of
+            technology and security. Together, we can create digital experiences
+            that not only captivate but also ensure a secure online environment.
+          </p>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Home;
- 
